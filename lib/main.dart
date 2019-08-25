77d1243800122724bd5c403bf5271ead301b1520
anthony <aalbertini95@gmail.com>
2019-08-25 16:40:47 +0200

    implement routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kiki_music/screens/home_screen.dart';
+import 'package:flutter_kiki_music/screens/playlist_details_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,12 +19,22 @@ class MyApp extends StatelessWidget {
         // - https://flutter.dev/docs/cookbook/design/themes
         // - https://api.flutter.dev/flutter/material/TextTheme-class.html
         textTheme: TextTheme(
-          headline: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.white),
+          headline: TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.white),
           body1: TextStyle(fontSize: 14.0, color: Colors.white),
           body2: TextStyle(fontSize: 12.0, color: Color(0x62FFFFFF)),
         ),
       ),
-      home: HomeScreen(title: 'Flutter Demo Home Page'),
+
+      // https://flutter.dev/docs/development/ui/navigation
+      // https://flutter.dev/docs/cookbook/navigation/named-routes#2-define-the-routes
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => HomeScreen(title: 'Home'),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/playlist': (context) => PlaylistDetailsScreen(title: 'Playlist'),
+      },
     );
   }
 }
