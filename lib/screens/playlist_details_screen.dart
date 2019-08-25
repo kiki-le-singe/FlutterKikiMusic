0caf0d4091aca6e8e501b0a1b5af8591749f3063
anthony <aalbertini95@gmail.com>
2019-08-25 16:39:30 +0200

    implement playlistDetailsScreen

---
+++ lib/screens/playlist_details_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PlaylistDetailsScreen extends StatefulWidget {
+  PlaylistDetailsScreen({this.title});
+
+  final String title;
+
+  @override
+  _PlaylistDetailsScreenState createState() => _PlaylistDetailsScreenState();
+}
+
+class _PlaylistDetailsScreenState extends State<PlaylistDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.grey[900],
+      ),
+      body: SingleChildScrollView(
+          padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+          child: Container(
+              color: Colors.lightBlue,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[Text("Coucou")],
+              ))),
+    );
+  }
+}
