02682a9516d77d10388a97c19f6ee6a0a4317cc5
anthony <aalbertini95@gmail.com>
2019-08-28 14:46:45 +0200

    add an error screen if there is no matched route

---
+++ lib/screens/error_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ErrorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Error'),
+      ),
+      body: Center(
+        child: Text('ERROR'),
+      ),
+    );
+  }
+}
