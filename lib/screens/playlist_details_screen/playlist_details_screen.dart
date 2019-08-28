d7b20f24b2754743b15d52622cfe16b2363db214
anthony <aalbertini95@gmail.com>
2019-08-28 14:50:05 +0200

    get and display title argument

--- lib/screens/playlist_details_screen/playlist_details_screen.dart
+++ lib/screens/playlist_details_screen/playlist_details_screen.dart
@@ -6,14 +6,20 @@ class PlaylistDetailsScreen extends StatefulWidget {
   final String title;
 
   @override
+  _PlaylistDetailsScreenState createState() => _PlaylistDetailsScreenState(title: title);
 }
 
 class _PlaylistDetailsScreenState extends State<PlaylistDetailsScreen> {
+  _PlaylistDetailsScreenState({this.title});
+
+  final String title;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        title: Text(title),
         backgroundColor: Colors.grey[900],
       ),
       body: SingleChildScrollView(
